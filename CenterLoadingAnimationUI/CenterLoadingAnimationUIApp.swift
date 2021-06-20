092cafa5e800b1625462a303b7222009e833f886
张亚飞 <672233580@qq.com>
2021-06-20 14:19:58 +0800

    Initial Commit

---
+++ CenterLoadingAnimationUI/CenterLoadingAnimationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenterLoadingAnimationUIApp.swift
+//  CenterLoadingAnimationUI
+//
+//  Created by 张亚飞 on 2021/6/20.
+//
+
+import SwiftUI
+
+@main
+struct CenterLoadingAnimationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
